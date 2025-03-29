a9af10856b78158efc04ba5cc3c443ddc0eabebf
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2025-03-29 09:55:22 -0300

    :sparkles:style(dashboard): Dashboard Page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+//as páginas layouts são exclusivamente para UI, no caso dessa pasta ele tem um ui que é compartilhado com os que estão dentro dessa pasta e em pastas que estão dentro dessa também, em resumo um layout compartilhado
+
+import SideNav from '@/app/ui/dashboard/sidenav';
+//  esse children serve para atribuir as outras pastas
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+// o usePathname ele serve para pegar o link que o usuário está atualmente, para podermos ultilizar para indicar a página onde ele está por exemplo colocando o link onde ele clicou em azul
+import { usePathname } from 'next/navigation';
+// nesse caso o clsx será ultilizado junto com o usePathname para deixar o link que foi clicado em azul
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +24,27 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
+
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // o link ele fica no lugar da tag a, a vantagem é que ele não atualiza a página toda quando clica em outro link
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
