76e7a85e17ce700e8bbe38c9f6db39a5ee7095b5
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2025-04-08 17:59:56 -0300

    :bug:fix: remoção de imports não utilizados

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import FluxoDigitalLogo from '@/app/ui/fluxo-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
--- app/ui/fluxo-logo.tsx
+++ app/ui/fluxo-logo.tsx
@@ -1,13 +1,17 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function FluxoDigitalLogo() {
   return (
     <div
       className={`flex items-flex-end justify-start md:items-center md:justify-center`}
     >
-      <img src="/logo.png" alt="Fluxo Digital logo" className="h-12 w-12 md:h-24 md:w-24" />
-      
+      <Image 
+        src="/logo.png" 
+        alt="Fluxo Digital logo" 
+        width={96}
+        height={96}
+        className="h-12 w-12 md:h-24 md:w-24" 
+      />
     </div>
   );
 }
